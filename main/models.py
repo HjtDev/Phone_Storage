c76e1504c7ede9636f534a2f0771a7f62294505f
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2024-11-29 15:14:58 +0330

    chore(models)
    -Updated models field names and added Meta classes

--- main/models.py
+++ main/models.py
@@ -3,11 +3,15 @@ from django.core.validators import MinValueValidator
 
 
 class Brand(models.Model):
-    name = models.CharField(verbose_name='Name', max_length=255)
-    nationality = models.CharField(verbose_name='Nationality', max_length=255)
+    name = models.CharField(verbose_name='Name', max_length=255, unique=True)
+    country = models.CharField(verbose_name='Country', max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
 
 
 class Phone(models.Model):
@@ -18,4 +22,12 @@ class Phone(models.Model):
     display_size = models.DecimalField(verbose_name='Display Size', max_digits=4, decimal_places=2, validators=[MinValueValidator(1)])
     made_in = models.CharField(verbose_name='Made In', max_length=255)
     is_available = models.BooleanField(verbose_name='Is Available', default=False)
+
+    def __str__(self):
+        return self.model
+
+    class Meta:
+        verbose_name = 'Phone'
+        verbose_name_plural = 'Phones'
+
 
